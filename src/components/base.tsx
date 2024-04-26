8509551e2bafc4ffcd19b422bcdf9beefefb0e73
Guilherme Oliveira <htoguilherme@gmail.com>
2024-04-26 17:14:14 +1200

    feat: add css reset

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -4,6 +4,7 @@ export const Base = ({ children }) => (
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Dev links</title>
+      <link href="/public/styles.css" rel="stylesheet" />
       <script
         src="https://unpkg.com/htmx.org@1.9.12/dist/htmx.js"
         integrity="sha384-qbtR4rS9RrUMECUWDWM2+YGgN3U4V4ZncZ0BvUcg9FGct0jqXz3PUdVpU1p0yrXS"
