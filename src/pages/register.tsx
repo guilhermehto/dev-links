115e6f656c22eb198bc1660daf003b85cf39a3c5
Guilherme Oliveira <htoguilherme@gmail.com>
2024-04-26 22:39:16 +1200

    feat: add name to register form inputs

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -13,41 +13,48 @@ export const Register = () => {
           <Logo />
         </span>
         <Card class="w-[476px]">
-          <Stack spacing="md">
-            <Stack>
-              <h1 class="text-4xl font-bold">Create account</h1>
-              <p class="text-gray">Let's get you started sharing your links!</p>
+          <form hx-post="/register">
+            <Stack spacing="md">
+              <Stack>
+                <h1 class="text-4xl font-bold">Create account</h1>
+                <p class="text-gray">
+                  Let's get you started sharing your links!
+                </p>
+              </Stack>
+              <Input
+                name="email"
+                label="Email address"
+                placeholder="e.g. alex@email.com"
+                rightElement={
+                  <span class="icon-[ph--envelope-simple-fill] h-full w-full" />
+                }
+              />
+              <Input
+                name="password"
+                label="Create password"
+                placeholder="At least 8 characters"
+                type="password"
+                rightElement={<span class="icon-[ph--lock-key-fill]" />}
+              />
+              <Input
+                name="confirm"
+                label="Confirm password"
+                placeholder="At least 8 characters"
+                type="password"
+                rightElement={<span class="icon-[ph--lock-key-fill]" />}
+              />
+              <p class="text-xs text-gray">
+                Password must contain at least 8 characters
+              </p>
+              <Button type="submit">Create new account</Button>
+              <p class="text-gray text-center">
+                Already have an account?{' '}
+                <a href="/login" class="text-purple">
+                  Login
+                </a>
+              </p>
             </Stack>
-            <Input
-              label="Email address"
-              placeholder="e.g. alex@email.com"
-              rightElement={
-                <span class="icon-[ph--envelope-simple-fill] h-full w-full" />
-              }
-            />
-            <Input
-              label="Create password"
-              placeholder="At least 8 characters"
-              type="password"
-              rightElement={<span class="icon-[ph--lock-key-fill]" />}
-            />
-            <Input
-              label="Confirm password"
-              placeholder="At least 8 characters"
-              type="password"
-              rightElement={<span class="icon-[ph--lock-key-fill]" />}
-            />
-            <p class="text-xs text-gray">
-              Password must contain at least 8 characters
-            </p>
-            <Button>Create new account</Button>
-            <p class="text-gray text-center">
-              Already have an account?{' '}
-              <a href="/login" class="text-purple">
-                Login
-              </a>
-            </p>
-          </Stack>
+          </form>
         </Card>
       </div>
     </Base>
