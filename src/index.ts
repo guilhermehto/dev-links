f5db77a40bff5264c7941f863a25f53a947ec828
Guilherme Oliveira <htoguilherme@gmail.com>
2024-04-26 22:09:33 +1200

    feat: add register page

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { Elysia } from 'elysia';
 import { Index } from './pages';
 import { Login } from './pages/login';
 import staticPlugin from '@elysiajs/static';
+import { Register } from './pages/register';
 
 const app = new Elysia()
   .use(html())
   .use(staticPlugin())
   .get('/', Index)
   .get('login', Login)
+  .get('register', Register)
   .listen(3000);
 
 console.log(
---
+++ src/pages/register.tsx
@@ -0,0 +1,55 @@
+import { Base } from '../components/base';
+import { Button } from '../components/button';
+import { Card } from '../components/card';
+import { Input } from '../components/input';
+import { Logo } from '../components/logo';
+import { Stack } from '../components/stack';
+
+export const Register = () => {
+  return (
+    <Base>
+      <div class="flex flex-col items-center">
+        <span class="pb-12">
+          <Logo />
+        </span>
+        <Card>
+          <Stack spacing="md">
+            <Stack>
+              <h1 class="text-4xl font-bold">Create account</h1>
+              <p class="text-gray">Let's get you started sharing your links!</p>
+            </Stack>
+            <Input
+              label="Email address"
+              placeholder="e.g. alex@email.com"
+              rightElement={
+                <span class="icon-[ph--envelope-simple-fill] h-full w-full" />
+              }
+            />
+            <Input
+              label="Create password"
+              placeholder="At least 8 characters"
+              type="password"
+              rightElement={<span class="icon-[ph--lock-key-fill]" />}
+            />
+            <Input
+              label="Confirm password"
+              placeholder="At least 8 characters"
+              type="password"
+              rightElement={<span class="icon-[ph--lock-key-fill]" />}
+            />
+            <p class="text-xs text-gray">
+              Password must contain at least 8 characters
+            </p>
+            <Button>Create new account</Button>
+            <p class="text-gray text-center">
+              Already have an account?{' '}
+              <a href="/login" class="text-purple">
+                Login
+              </a>
+            </p>
+          </Stack>
+        </Card>
+      </div>
+    </Base>
+  );
+};
