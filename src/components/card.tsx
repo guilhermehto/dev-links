1bbdf80af72f02cd11e393eba2f178da9a38570a
Guilherme Oliveira <htoguilherme@gmail.com>
2024-04-26 22:13:44 +1200

    feat: accept classes from card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,11 @@
+import clsx from 'clsx';
+
 type CardProps = {
   children: JSX.Element | string;
+  class?: string;
 };
 
-export const Card = ({ children }: CardProps) => (
-  <div class="p-10 bg-white rounded-lg">{children}</div>
-);
+export const Card = ({ children, class: className }: CardProps) => {
+  const classes = clsx('p-10 bg-white rounded-lg', className);
+  return <div class={classes}>{children}</div>;
+};
