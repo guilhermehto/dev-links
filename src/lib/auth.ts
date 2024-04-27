9446a844d539099017186e00d9b11a15003d1cf5
Guilherme Oliveira <htoguilherme@gmail.com>
2024-04-27 18:23:40 +1200

    feat: add lucia helpers

---
+++ src/lib/auth.ts
@@ -0,0 +1,33 @@
+import { Lucia } from 'lucia';
+import { LibSQLAdapter } from '@lucia-auth/adapter-sqlite';
+import { getClient } from '../db';
+
+export const adapter = new LibSQLAdapter(getClient(), {
+  user: 'user',
+  session: 'session',
+});
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      username: attributes.email,
+    };
+  },
+});
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+    UserId: number;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  email: string;
+}
