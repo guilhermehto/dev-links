120f4e4b55eb09f4b3c74ca81ce7a4532afc5e35
Guilherme Oliveira <htoguilherme@gmail.com>
2024-05-09 08:49:53 +1200

    feat: outline button variant

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,30 @@
+import clsx from 'clsx';
+
 type ButtonProps = {
   children: string;
   type?: 'submit' | 'button';
+  variant?: 'default' | 'outline';
 };
 
-export const Button = ({ children, type = 'button' }: ButtonProps) => {
+export const Button = ({
+  children,
+  type = 'button',
+  variant = 'default',
+}: ButtonProps) => {
+  const classes = clsx(
+    'text-white rounded-lg py-2.5 px-7',
+    {
+      'bg-purple active:bg-purple-hover active:drop-shadow':
+        variant === 'default',
+    },
+    {
+      'border text-purple font-bold border-purple active:bg-purple-light':
+        variant === 'outline',
+    },
+  );
+
   return (
-    <button
-      class="text-white rounded-lg bg-purple py-3 active:bg-purple-hover active:drop-shadow"
-      type={type}
-    >
+    <button class={classes} type={type}>
       {children}
     </button>
   );
