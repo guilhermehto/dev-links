72553a17c16d38f1736bbd308e8657ac39d371e2
Guilherme Oliveira <htoguilherme@gmail.com>
2024-05-28 22:02:02 +1200

    feat: add Row component

---
+++ src/components/row.tsx
@@ -0,0 +1,21 @@
+import { twMerge } from 'tailwind-merge';
+
+type RowProps = {
+  children: JSX.Element | JSX.Element[];
+  spacing?: 'sm' | 'md' | 'lg';
+};
+
+export const Row = ({ children, spacing = 'sm' }: RowProps) => {
+  return (
+    <div
+      class={twMerge(
+        'flex flex-row',
+        spacing === 'sm' && 'gap-2',
+        spacing === 'md' && 'gap-6',
+        spacing === 'lg' && 'gap-10',
+      )}
+    >
+      {children}
+    </div>
+  );
+};
